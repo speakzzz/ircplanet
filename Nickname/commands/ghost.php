8352274f906c62e915aaa677b91d3995391143c8
Felix Alcantara <speakz@speakz.org>
2025-11-27 08:58:13 -0500

    Update ghost.php

--- Nickname/commands/ghost.php
+++ Nickname/commands/ghost.php
@@ -3,20 +3,18 @@
  * ircPlanet Services for ircu
  * Copyright (c) 2005 Brian Cline.
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ * * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
 
  * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
+ * this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * 3. Neither the name of ircPlanet nor the names of its contributors may be
- *    used to endorse or promote products derived from this software without 
- *    specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * used to endorse or promote products derived from this software without 
+ * specific prior written permission.
+ * * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
@@ -29,10 +27,11 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 	
+	$password = null;
+
 	if ($user->isLoggedIn()) {
 		$user_name = $user->getAccountName();
-		$account = $this->getAccount($user_name);
-		$password_md5 = $account->getPassword();
+		// Logged in users don't need to provide a password to ghost themselves
 	}
 	elseif ($cmd_num_args < 2) {
 		$bot->noticef($user, "%sSyntax:%s %s %s",
@@ -42,7 +41,7 @@
 	}
 	else {
 		$user_name = $pargs[1];
-		$password_md5 = md5($pargs[2]);
+		$password = $pargs[2];
 	}
 	
 	
@@ -68,9 +67,13 @@
 	
 	$targetNick = $target->getNick();
 	
-	if ($account->getPassword() != $password_md5) {
-		$bot->notice($user, "Invalid password!");
-		return false;
+	// Modernization: Use password_verify instead of md5 comparison
+	// If the user is logged in, they are trusted (checked above)
+	if (!$user->isLoggedIn()) {
+		if (!password_verify($password, $account->getPassword())) {
+			$bot->notice($user, "Invalid password!");
+			return false;
+		}
 	}
 	
 	$user_name = $account->getName();
@@ -94,5 +97,3 @@
 		$user->setAccountName($user_name);
 		$user->setAccountId($account->getId());
 	}
-	
-
