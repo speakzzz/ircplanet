76f1760f3c0c0c069e7fabb0fd40a9780de4841c
Felix Alcantara <speakz@speakz.org>
2025-11-27 08:29:13 -0500

    Security Modernization (Password Hashing)

--- Nickname/commands/login.php
+++ Nickname/commands/login.php
@@ -3,20 +3,18 @@
  * ircPlanet Services for ircu
  * Copyright (c) 2005 Brian Cline.
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ * * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
 
  * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
+ * this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * 3. Neither the name of ircPlanet nor the names of its contributors may be
- *    used to endorse or promote products derived from this software without 
- *    specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * used to endorse or promote products derived from this software without 
+ * specific prior written permission.
+ * * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
@@ -39,9 +37,8 @@
 	}
 	
 	if ($account = $this->getAccount($user_name)) {
-		$password_md5 = md5($password);
-		
-		if ($account->getPassword() != $password_md5) {
+		// Modernization: Use password_verify instead of md5 comparison
+		if (!password_verify($password, $account->getPassword())) {
 			$bot->notice($user, "Invalid password!");
 			return false;
 		}
@@ -77,5 +74,3 @@
 	else {
 		$bot->notice($user, "No such account!");
 	}
-
-
