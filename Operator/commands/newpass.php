86579c2341b2a0c2dd9c13ec861a18dbdd7d2b5a
Felix Alcantara <speakz@speakz.org>
2025-11-27 09:06:50 -0500

    This version replaces the insecure md5 hashing with password_hash, ensuring that new passwords set by operators are stored securely using Bcrypt.

--- Operator/commands/newpass.php
+++ Operator/commands/newpass.php
@@ -3,20 +3,18 @@
  * ircPlanet Services for ircu
  * Copyright (c) 2005 Brian Cline.
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ * * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
 
  * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
+ * this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * 3. Neither the name of ircPlanet nor the names of its contributors may be
- *    used to endorse or promote products derived from this software without 
- *    specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * used to endorse or promote products derived from this software without 
+ * specific prior written permission.
+ * * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
@@ -30,12 +28,12 @@
  */
 	
 	$password = $pargs[1];
-	$password_md5 = md5($password);
+	
+	// Modernization: Use password_hash instead of md5
+	$password_hash = password_hash($password, PASSWORD_DEFAULT);
 
 	$account = $this->getAccount($user->getAccountName());
-	$account->setPassword($password_md5);
+	$account->setPassword($password_hash);
 	$account->save();
 			
 	$bot->notice($user, "Your password has been changed.");
-
-
