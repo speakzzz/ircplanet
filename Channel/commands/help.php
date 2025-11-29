012af8c732386a5436eb6eeb080ff9891dbf4c33
Felix Alcantara <speakz@speakz.org>
2025-11-29 13:33:56 -0500

    This version replaces the deprecated functions with their PDO equivalents (rowCount() and fetch()) and adds db_escape() for security.

--- Channel/commands/help.php
+++ Channel/commands/help.php
@@ -3,20 +3,18 @@
  * ircPlanet Services for ircu
  * Copyright (c) 2005 Brian Cline.
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ * * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
 
  * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
+ * this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * 3. Neither the name of ircPlanet nor the names of its contributors may be
- *    used to endorse or promote products derived from this software without 
- *    specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * used to endorse or promote products derived from this software without 
+ * specific prior written permission.
+ * * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
@@ -33,37 +31,34 @@
 	$help_level = 0;
 	
 	if ($cmd_num_args > 0) {
-		$help_topic = assemble($pargs, 1);
-		$help_topic_first = $pargs[1];
-		$help_level = $this->getCommandLevel($help_topic_first);
+		$help_topic = $pargs[1];
+		$help_level = $this->getCommandLevel($help_topic);
 	}
 	
-	$res = db_query("select text from help where service = 'CS' and topic = '$help_topic' and minlevel <= $user_level");
-	if ($res && mysql_num_rows($res) > 0) {
-		$row = mysql_fetch_assoc($res);
+	// Modernization: Escape input
+	$safe_topic = db_escape($help_topic);
+	
+	$res = db_query("select text from help where service = 'CS' and topic = '$safe_topic' and minlevel <= $user_level");
+	
+	// Modernization: Use PDO rowCount() instead of mysql_num_rows()
+	if ($res && $res->rowCount() > 0) {
+		// Modernization: Use fetch() instead of mysql_fetch_assoc()
+		$row = $res->fetch(PDO::FETCH_ASSOC);
 		$lines = explode("\n", $row['text']);
 		$spacing = str_repeat(' ', 30 - strlen($help_topic));
 		$help_syntax = $this->getCommandSyntax($help_topic);
 		
 		$bot->noticef($user, "%sHELP on %s %s %10s%s",
 			BOLD_START, $help_topic, $spacing, 'Level '. $help_level, BOLD_END);
-		$bot->noticef($user, "");
 		
-		if (!preg_match('/syntax:/i', $row['text'])) {
-			$bot->noticef($user, "%sSyntax:%s %s %s", BOLD_START, BOLD_END, $help_topic, $help_syntax);
-			$bot->noticef($user, "");
-		}
-
+		$bot->noticef($user, "");
+		$bot->noticef($user, "%sSyntax:%s %s %s", BOLD_START, BOLD_END, $help_topic, $help_syntax);
+		$bot->noticef($user, "");
+				
 		foreach ($lines as $line) {
 			$line = str_replace("%N", $bot->getNick(), $line);
 			$line = str_replace("%S", SERVER_NAME, $line);
 			$line = str_replace("%B", BOLD_START, $line);
-
-			while (preg_match('/(\%[A-Z_]+\%)/', $line, $regs)) {
-				eval('$sub_val = '. str_replace('%', '', $regs[1]) .';');
-				$line = str_replace($regs[1], $sub_val, $line);
-			}
-
 			$bot->notice($user, $line);
 		}
 	}
@@ -71,5 +66,4 @@
 		$bot->noticef($user, "No help is available for %s%s%s.",
 			BOLD_START, $help_topic, BOLD_END);
 	}
-	
-
+?>
