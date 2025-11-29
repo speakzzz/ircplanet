d2ebbe0bf06f0782d7ecb3e6a2a3c02ccff3604e
Felix Alcantara <speakz@speakz.org>
2025-11-29 15:01:58 -0500

    Update ss.php

--- Stat/ss.php
+++ Stat/ss.php
@@ -3,20 +3,18 @@
  * ircPlanet Services for ircu
  * Copyright (c) 2005 Brian Cline.
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ * * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
 
  * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
+ * this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * 3. Neither the name of ircPlanet nor the names of its contributors may be
- *    used to endorse or promote products derived from this software without 
- *    specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * used to endorse or promote products derived from this software without 
+ * specific prior written permission.
+ * * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
@@ -107,10 +105,18 @@
 				$acct_id = $user_obj->getAccountId();
 			}
 			
-			$res = db_query("select `level` from `ss_admins` where user_id = ". $acct_id);
-			if ($res && mysql_num_rows($res) > 0) {
-				$level = mysql_result($res, 0);
-				mysql_free_result($res);
+            // Modernization: Escape input
+            if (function_exists('db_escape')) {
+                $acct_id = db_escape($acct_id);
+            } else {
+                $acct_id = addslashes($acct_id);
+            }
+
+			$res = db_query("select `level` from `ss_admins` where user_id = '$acct_id'");
+			
+            // Modernization: Use rowCount() and fetchColumn()
+            if ($res && $res->rowCount() > 0) {
+				$level = $res->fetchColumn(0);
 				return $level;
 			}
 			
@@ -120,5 +126,3 @@
 	}
 	
 	$ss = new StatService();
-
-
