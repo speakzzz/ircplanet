4ee7216db73e780d11898df44985e1bc59d53355
Felix Alcantara <speakz@speakz.org>
2025-12-05 20:23:05 -0500

    Update fakehost.php

--- Operator/commands/fakehost.php
+++ Operator/commands/fakehost.php
@@ -29,12 +29,63 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-	if (!($target = $this->getUserByNick($pargs[1]))) {
-		$bot->noticef($user, 'There is no user named %s.', $pargs[1]);
+	if ($cmd_num_args < 2) {
+		$bot->notice($user, "Syntax: FAKEHOST <nick> <host>");
 		return false;
 	}
+
+	$target_name = $pargs[1];
+	$host = $pargs[2];
+
+	// 1. Find the account
+	$account = $this->getAccount($target_name);
+	if (!$account) {
+		$bot->noticef($user, "Account %s does not exist.", $target_name);
+		return false;
+	}
+
+	// 2. Validate Host Length
+	if (strlen($host) > 63) {
+		$bot->notice($user, "Fakehost is too long (Max 63 chars).");
+		return false;
+	}
+
+	// 3. Save to Database
+	// This uses the updated DB_User class which uses PDO prepared statements internally via save()
+	$account->setFakehost($host);
+	$account->save();
+
+	// 4. Apply to live user if online
+	$target_user = $this->getUserByNick($target_name);
 	
-	$this->sendf(FMT_FAKEHOST, SERVER_NUM, $target->getNumeric(), $pargs[2]);
-	$target->addMode(UMODE_FAKEHOST);
-	$target->setFakehost($pargs[2]);
+	// If looking up by nick failed, search by account ID (user might be on a different nick)
+	if (!$target_user) {
+		foreach ($this->users as $u) {
+			if ($u->getAccountId() == $account->getId()) {
+				$target_user = $u;
+				break;
+			}
+		}
+	}
+
+	if ($target_user) {
+		// Update memory
+		$target_user->setFakehost($host);
+
+		// Broadcast change to network (FMT_FAKEHOST = "%s FA %s %s")
+		$this->sendf(FMT_FAKEHOST, SERVER_NUM, $target_user->getNumeric(), $host);
+		
+		// Notify User
+		$bot->noticef($target_user, "Your vHost has been changed to: %s", $host);
+		
+		if (!$target_user->hasMode(UMODE_HIDDENHOST)) {
+			$bot->notice($target_user, "Type /mode " . $target_user->getNick() . " +x to activate it.");
+		}
+	}
+
+	$bot->noticef($user, "Updated vHost for account %s.", $account->getName());
 	
+	// Log to Wallops
+	$this->sendf(FMT_WALLOPS, SERVER_NUM, sprintf("vHost for %s changed to %s by %s", 
+		$account->getName(), $host, $user->getNick()));
+?>
